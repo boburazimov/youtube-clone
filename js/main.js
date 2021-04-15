f5c07ef5f4d71c79707647d4874ea868a7fec11b
Bobur Azimov <30525017+boburazimov@users.noreply.github.com>
2021-04-15 22:15:15 +0500

    Channel-Content

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-const swiper = new Swiper('.channel-slider', {
+const swiper1 = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
 
-    slidesPerView: 6,
-    spaceBetween: 40,
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+        },
+        1600: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 4
+        },
+        1100: {
+            slidesPerView: 3
+        },
+        800: {
+            slidesPerView: 2
+        },
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.channel-button-next',
         prevEl: '.channel-button-prev',
+    },
+});
+
+const swiper2 = new Swiper('.recommended-slider', {
+    // Optional parameters
+    loop: true,
+
+    slidesPerView: 2,
+    spaceBetween: 30,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.recommended-button-next',
+        prevEl: '.recommended-button-prev',
     },
 });
